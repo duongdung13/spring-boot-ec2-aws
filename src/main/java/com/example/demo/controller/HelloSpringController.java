3b7ba986589f0a0a69f1ff9243ba236f252745c1
Duong Dung <dungdcth@gmail.com>
2025-05-07 23:00:03 +0700

    Hello

---
+++ src/main/java/com/example/demo/controller/HelloSpringController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloSpringController {
+
+    @GetMapping()
+    ResponseEntity<?> sayHello() {
+        Map<String, Object> result = new HashMap<>();
+        result.put("data", "Hello Spring!");
+        result.put("message", "Success");
+        result.put("status", 200);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(result);
+    }
+
+}
